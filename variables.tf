b11da0ffb4ba0157eda33ca386102413531490ba
Chaithanya-AS <57350830+Chaithanya-AS@users.noreply.github.com>
2019-11-09 14:23:09 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,3 +15,11 @@ variable "webapplocs" {
     type        = "list"
     default     = []
 }
+
+variable "subscriptionId" {
+    default     =   "32c32163-f026-41ed-9503-3b0ee6ec5540"
+}
+
+variable "clientSecret" {
+    default     =   "b9543670-34cb-410d-ad75-fb29e77964cc"
+}
