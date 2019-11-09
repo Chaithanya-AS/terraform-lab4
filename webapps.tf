98653b6d59a9be3f620b9a8e5d0d3a8edba07466
Chaithanya-AS <57350830+Chaithanya-AS@users.noreply.github.com>
2019-11-09 14:22:41 +0530

    Update webapps.tf

--- webapps.tf
+++ webapps.tf
@@ -40,3 +40,10 @@ resource "azurerm_app_service" "citadel" {
 output "webapp_ids" {
   value = "${azurerm_app_service.citadel.*.id}"
 }
+
+provider "azurerm" {
+  subscription_id = "${var.subscriptionId}"
+  client_id       = "b9543670-34cb-410d-ad75-fb29e77964cc"
+  client_secret   = "${var.clientSecret}"
+  tenant_id       = "f66ccb58-5436-40de-ae8b-e951e03cb2b3"
+}
